0ca3016dda00ef4a5ee9462d4f3428c8755855ac
hoss <mecheng.hoss@gmail.com>
2023-07-18 06:31:04 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,20 +12,19 @@ int print_sign(int n)
     if (n > 0)
     {
         _putchar('+');
-        return 1;
+        return (1);
     }
     else if (n == 0)
     {
         _putchar('0');
-        return 0;
+        return (0);
     }
     else if (n < 0)
     {
         _putchar('-');
-        return -1;
+        return (-1);
     }
 
-    // Default case if none of the conditions are met
+    /* Default case if none of the conditions are met */
     return 0;
-}
 }
