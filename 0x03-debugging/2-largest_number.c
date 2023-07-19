c9a53fc687787d6e9afadb8b99f6656583124ee0
hoss <mecheng.hoss@gmail.com>
2023-07-19 00:09:39 -0700

    0x03-debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,15 +8,12 @@
 */
 int largest_number(int a, int b, int c)
 {
-    int largest = a;
-
-    if (b > largest) {
-        largest = b;
-    }
-
-    if (c > largest) {
-        largest = c;
-    }
-
-    return largest;
+int largest = a;
+if (b > largest) {
+largest = b;
+}
+if (c > largest) {
+largest = c;
+}
+return largest;
 }
