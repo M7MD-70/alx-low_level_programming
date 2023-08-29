d61bd42d9fe9d13d4ec5a7b87a4a19869ddb1d2c
hoss <mecheng.hoss@gmail.com>
2023-08-29 06:49:43 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,8 +10,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Function prototype for print_listint */
+/* Function prototypes */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
 
