d0347d400ff23bb87cc99c7b51af833398f5e780
hoss <mecheng.hoss@gmail.com>
2023-08-29 06:35:04 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
-#include <stdio.h>  /* Include this line to fix the implicit declaration error */
+#include <stdio.h> /* Include this line to fix implicit declaration error */
 #include "lists.h"
 
 /**
  * print_listint - Prints all elements of a listint_t linked list.
  * @h: Pointer to the head of the linked list.
- * Return: The number of nodes in the linked list.
+ * Return: Number of nodes in the linked list.
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    return count;
+	return (count);
 }
